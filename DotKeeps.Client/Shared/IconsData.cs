4375aac79984052aeb09bebcb455ea2ada8d3ed1
José Israel <hswolf@proton.me>
2024-02-04 17:41:32 -0300

    feat(client): Cleaned up the default app.
    Added a tailwind preflight stylesheet.
    
    Created all the main pages.
    
    Updated the navbar to be simple and responsive.

---
+++ DotKeeps.Client/Shared/IconsData.cs
@@ -0,0 +1,8 @@
+namespace DotKeeps.Client.Shared;
+
+public class IconsData
+{
+  public static readonly string HamburguerMenu = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"1em\" height=\"1em\" viewBox=\"0 0 32 32\"><path fill=\"currentColor\" d=\"M4 6h24v2H4zm0 18h24v2H4zm0-12h24v2H4zm0 6h24v2H4z\"/></svg>";
+  public static readonly string FloppyDisk = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"1em\" height=\"1em\" viewBox=\"0 0 32 32\"><path fill=\"currentColor\" d=\"m27.71 9.29l-5-5A1 1 0 0 0 22 4H6a2 2 0 0 0-2 2v20a2 2 0 0 0 2 2h20a2 2 0 0 0 2-2V10a1 1 0 0 0-.29-.71M12 6h8v4h-8Zm8 20h-8v-8h8Zm2 0v-8a2 2 0 0 0-2-2h-8a2 2 0 0 0-2 2v8H6V6h4v4a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V6.41l4 4V26Z\"/></svg>";
+  public static readonly string MedievalCastle = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"1em\" height=\"1em\" viewBox=\"0 0 24 24\"><path fill=\"currentColor\" d=\"M1 21V9h2v2h2V3h2v2h2V3h2v2h2V3h2v2h2V3h2v8h2V9h2v12h-9v-3q0-.825-.587-1.412T12 16q-.825 0-1.412.588T10 18v3zm8-9h2V9H9zm4 0h2V9h-2z\"/></svg>";
+}
